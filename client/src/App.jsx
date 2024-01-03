f5daa34c7171dda7ea47a7a6361d9386b03d88ac
Mathias007 <mathias.najlepszy@gmail.com>
2024-01-03 20:23:43 +0100

    [FE] Build the first Register form.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
+import Register from "./Register";
+
 function App() {
-  
   return (
-    <div className='bg-red-500'>test</div>
+    <Register />
   )
 }
 
---
+++ client/src/Register.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className="bg-blue-50 h-screen flex items-center">
+            <form className="w-64 mx-auto mb-12">
+                <input 
+                    value={username} 
+                    type="text" 
+                    onChange={e => setUsername(e.target.value)} 
+                    placeholder="username" 
+                    className="block w-full rounded-sm p-2 mb-2" 
+                />
+                <input 
+                    value={password} 
+                    type="password" 
+                    onChange={e => setPassword(e.target.value)} 
+                    placeholder="password" 
+                    className="block w-full rounded-sm p-2 mb-2" 
+                />
+                <button 
+                    className="bg-blue-500 text-white block w-full rounded-md-sm p-2">
+                        Register
+                </button>
+            </form>
+        </div>
+    )
+}
