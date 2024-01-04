addef0220ec754053a44f29eb0b2f45373a61b77
Mathias007 <mathias.najlepszy@gmail.com>
2024-01-04 20:48:28 +0100

    [FE] Handle gray dot for offline users.

--- client/src/Avatar.jsx
+++ client/src/Avatar.jsx
@@ -1,6 +1,6 @@
 export default function Avatar({userId, username, online}) {
     const colors = ['bg-teal-200', 'bg-red-200', 'bg-green-200', 
-                    'bg-purple-200', 'bg-blue-200', 'bg-yellow-200'];
+                    'bg-purple-200', 'bg-pink-200', 'bg-yellow-200'];
 
     const userIdBase10 = parseInt(userId, 16);
     const colorIndex = userIdBase10 % colors.length;
@@ -9,9 +9,7 @@ export default function Avatar({userId, username, online}) {
     return (
         <div className={"w-8 h-8 relative rounded-full text-center flex items-center " + color}>
             <div className="text-center w-full opacity-70">{username[0]}</div>
-            {online && (
-            <div className="absolute w-3 h-3 bg-green-400 bottom-0 right-0 rounded-full border border-white"></div>
-            )}
+            <div className={`absolute w-3 h-3 bg-${online ? "green": "gray"}-400 bottom-0 right-0 rounded-full border border-white`}></div>
         </div>
     );
 }
