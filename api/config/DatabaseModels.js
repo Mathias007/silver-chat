62fe208c544564bcd691f7026166b3b55c0ed729
Mathias007 <mathias.najlepszy@gmail.com>
2024-01-10 21:58:29 +0100

    [BE] Create DatabaseModels and WebSocketVariables files.

---
+++ api/config/DatabaseModels.js
@@ -0,0 +1,4 @@
+export const DatabaseModels = {
+    USER: "User",
+    MESSAGE: "Message",
+};
---
+++ api/config/WebSocketVariables.js
@@ -0,0 +1,10 @@
+export const wssListeners = {
+    CONNECTION: "connection",
+    PONG: "pong",
+    MESSAGE: "message",
+};
+
+export const wssIntervals = {
+    CONNECTION_TIMER_INTERVAL: 5000,
+    DEATH_TIMER_INTERVAL: 1000,
+};
