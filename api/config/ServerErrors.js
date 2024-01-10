13052306c00fa906c1fb14abd940a394c8bb49e2
Mathias007 <mathias.najlepszy@gmail.com>
2024-01-10 21:43:02 +0100

    [BE] Create ServerErrors file.

---
+++ api/config/ServerErrors.js
@@ -0,0 +1,13 @@
+export const ServerErrors = {
+    INTERNAL_SERVER_ERROR: "Something went wrong",
+    REJECT_NO_TOKEN: "No token",
+    AUTHORIZATION_GENERAL_ERROR: "Unauthorized",
+    WRONG_CREDENTIAL_ERROR: "Wrong login or password",
+    LOGOUT_MESSAGE: "Logout",
+    USER_ALREADY_EXISTS_ERROR:
+        "User with the provided username already exists.",
+    REGISTRATION_ERROR: "An error occurred during registration.",
+    MESSAGE_CREATED: "created message",
+    FILE_SAVED: "file saved:",
+    FILE_SIZE: "Size:",
+};
